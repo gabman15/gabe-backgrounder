001fc4d2d2efbdac5f490954a8dce9e067e48f85
gabman15 <gabrielmcninch@gmail.com>
2025-03-12 19:26:13 -0400

    args for config file and imgdir + handle if no previous img dir

--- backgrounder.py
+++ backgrounder.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python
+
+import argparse
 import json
 import requests
 from base64 import b64encode
 import random
 import os
+
+def set_bg(img):
+    os.system("feh --bg-max "+img)
+
 
 def get_total(szuru_api_url: str, headers: str) -> int:
     query_url = szuru_api_url + '/posts/?query=gabe_background'
@@ -71,29 +78,48 @@ def gen_random_img(szuru_url, headers, loc, alt_img, total) -> str:
             break
     return img
                     
-def _main():
-    img_loc = "/home/lord_gabem/Pictures/backgrounds"
-    bg_file = open(img_loc+'/bg.json','r')
-    config = open("config.json",'r')
+def main():
+    parser = argparse.ArgumentParser(prog='backgrounder')
+    parser.add_argument('-c', '--config', default="config.json")
+    parser.add_argument('-d', '--directory', default="/tmp/backgrounder")
+    args = parser.parse_args()
+    img_loc = args.directory
+
+    config = open(args.config,'r')
     config_data = json.load(config)
     url = config_data['url']
     user = config_data['user']
     token = config_data['token']
-    
+
+    # directory = os.path.dirname(img_loc)
+    os.makedirs(img_loc, exist_ok=True)
+
+    if(not os.path.isfile(img_loc+'/bg.json')):
+        bg_file = open(img_loc+'/bg.json','w')
+        bg_file.write(json.dumps({'current':0}))
+        bg_file.close()
+    bg_file = open(img_loc+'/bg.json','r')
     bg_data = json.load(bg_file)
 
-
     if(bg_data['current'] == 1):
-        os.system("feh --bg-max "+img_loc+bg_data['2'])
+        set_bg(img_loc+bg_data['2'])
         os.system("rm "+img_loc+bg_data['1'])
         bg_data['1'] = download_start(url, user, token, img_loc, bg_data['2'])
         bg_data['current'] = 2
-    else:
-        os.system("feh --bg-max "+img_loc+bg_data['1'])
+    elif(bg_data['current'] == 2):
+        set_bg(img_loc+bg_data['1'])
         os.system("rm "+img_loc+bg_data['2'])
         bg_data['2'] = download_start(url, user, token, img_loc, bg_data['1'])
         bg_data['current'] = 1
+    else:
+        bg_data['1'] = download_start(url, user, token, img_loc, "")
+        set_bg(img_loc+bg_data['1'])
+        bg_data['2'] = download_start(url, user, token, img_loc, bg_data['1'])
+        bg_data['current'] = 1
+
     bg_file = open(img_loc+'/bg.json','w')
     json.dump(bg_data, bg_file)
     
-_main()
+
+if __name__ == "__main__":
+    main()
