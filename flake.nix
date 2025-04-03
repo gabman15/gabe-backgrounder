dcec25683aacb603e897c70e81db5dcbbe221978
gabman15 <gabrielmcninch@gmail.com>
2025-04-02 22:33:06 -0400

    flake replace swaymsg with nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,12 +6,19 @@
     packages.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in rec {
-      gabe-backgrounder = pkgs.writers.writePython3Bin "gabe-backgrounder" {
+      gabe-backgrounder = (pkgs.writers.writePython3Bin "gabe-backgrounder" {
         libraries = with pkgs; [
           python312Packages.requests
         ];
         doCheck = false;
-      } (builtins.readFile ./backgrounder.py);
+      } (builtins.readFile ./backgrounder.py))
+        .overrideAttrs (old: {
+          buildCommand =
+            old.buildCommand
+            + ''
+              substituteInPlace $out/bin/gabe-backgrounder --replace 'swaymsg' '${pkgs.sway}/bin/swaymsg' 
+            '';
+        });
       default = gabe-backgrounder;
     };
   };
