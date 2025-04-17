791442ba5fae508f4bfd0fa8565ffcf494c06af3
gabman15 <gabrielmcninch@gmail.com>
2025-04-17 17:53:46 -0400

    check connection before running

--- backgrounder.py
+++ backgrounder.py
@@ -6,6 +6,7 @@ import requests
 from base64 import b64encode
 import random
 import os
+import sys
 
 def set_bg(img):
     if (os.getenv('WAYLAND_DISPLAY') is not None):
@@ -13,13 +14,13 @@ def set_bg(img):
     else:
         os.system("feh --bg-max " + img)
 
-def get_total(szuru_api_url: str, headers: str) -> int:
+def get_total(szuru_api_url: str, headers: dict) -> int:
     query_url = szuru_api_url + '/posts/?query=gabe_background'
     response_json = requests.get(query_url, headers=headers)
     response = response_json.json()
     return response['total']
 
-def download_post(szuru_url: str, headers: str, num: int, loc: str, alt_img: str) -> str:
+def download_post(szuru_url: str, headers: dict, num: int, loc: str, alt_img: str) -> str:
     szuru_api_url = szuru_url + '/api'
     offset = num - (num % 100)
     query_url = szuru_api_url + '/posts/?offset=' + str(offset) + '&query=gabe_background%20sort%3Arandom&limit=100'
@@ -44,7 +45,7 @@ def download_post(szuru_url: str, headers: str, num: int, loc: str, alt_img: str
 
     return filename
 
-def encode_auth_headers(user: str, token: str) -> str:
+def encode_auth_headers(user: str, token: str) -> dict:
     """Creates an authentication header from the user and token.
 
     This header is needed to interact with the szurubooru API.
@@ -54,18 +55,14 @@ def encode_auth_headers(user: str, token: str) -> str:
         szuru_token (str): The API token from `szuru_user`.
 
     Returns:
-        str: The encoded base64 authentication header.
+        dict: The request header with the encoded base64 authentication token.
     """
 
-    return b64encode(f'{user}:{token}'.encode()).decode('ascii')
+    return {'Content-Type': 'application/json', 'Accept': 'application/json',
+               'Authorization': 'Token ' + b64encode(f'{user}:{token}'.encode()).decode('ascii')}
 
-def download_start(szuru_url: str, szuru_user: str, szuru_token: str, loc: str, alt_img: str) -> str:
+def download_start(szuru_url: str, headers: dict, loc: str, alt_img: str) -> str:
     szuru_api_url = szuru_url + '/api'
-
-    token = encode_auth_headers(szuru_user, szuru_token)
-
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
-               'Authorization': 'Token ' + token}
 
     total = get_total(szuru_api_url, headers)
 
@@ -81,6 +78,13 @@ def gen_random_img(szuru_url, headers, loc, alt_img, total) -> str:
             break
     return img
 
+def check_connection(szuru_url, headers) -> bool:
+    try:
+        requests.get(szuru_url + '/api/info', headers=headers)
+        return True
+    except:
+        return False
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='backgrounder')
     parser.add_argument('-c', '--config', default="config.json")
@@ -93,6 +97,11 @@ if __name__ == "__main__":
     url = config_data['url']
     user = config_data['user']
     token = config_data['token']
+
+    headers = encode_auth_headers(user, token)
+
+    if (not check_connection(url, headers)):
+        sys.exit("Couldn't connect to szurubooru")
 
     # directory = os.path.dirname(img_loc)
     os.makedirs(img_loc, exist_ok=True)
@@ -107,17 +116,17 @@ if __name__ == "__main__":
     if (bg_data['current'] == 1):
         set_bg(img_loc + bg_data['2'])
         os.remove(img_loc + bg_data['1'])
-        bg_data['1'] = download_start(url, user, token, img_loc, bg_data['2'])
+        bg_data['1'] = download_start(url, headers, img_loc, bg_data['2'])
         bg_data['current'] = 2
     elif (bg_data['current'] == 2):
         set_bg(img_loc + bg_data['1'])
         os.remove(img_loc + bg_data['2'])
-        bg_data['2'] = download_start(url, user, token, img_loc, bg_data['1'])
+        bg_data['2'] = download_start(url, headers, img_loc, bg_data['1'])
         bg_data['current'] = 1
     else:
-        bg_data['1'] = download_start(url, user, token, img_loc, "")
+        bg_data['1'] = download_start(url, headers, img_loc, "")
         set_bg(img_loc + bg_data['1'])
-        bg_data['2'] = download_start(url, user, token, img_loc, bg_data['1'])
+        bg_data['2'] = download_start(url, headers, img_loc, bg_data['1'])
         bg_data['current'] = 1
 
     bg_file = open(img_loc + '/bg.json', 'w')
